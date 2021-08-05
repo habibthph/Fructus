c527074b9c838751effc8dff34c9d13d1ad1c00d
Patrick Habib <Patrick.Habib@yahoo.com>
2021-08-04 20:23:11 -0400

    Fruits List View

--- Fructus/App/FruitRowView.swift
+++ Fructus/App/FruitRowView.swift
@@ -21,6 +21,16 @@ struct FruitRowView: View {
                 .shadow(color: Color(red:0, green:0, blue:0, opacity: 0.3), radius: 3, x: 2, y: 2)
                 .background(LinearGradient(gradient:Gradient( colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom))
                 .cornerRadius(8)
+            
+            VStack(alignment: .leading, spacing: 5){
+                Text(fruit.title)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                
+                Text(fruit.headline)
+                    .font(.caption)
+                    .foregroundColor(Color.secondary)
+            }
         }//: HStack
     }
 }
